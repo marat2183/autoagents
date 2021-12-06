9d4c4aab52f2df2550b320f338d866021b652624
marat2183 <marat.grig318@gmail.com>
2021-12-06 13:31:33 +0300

    add listeners for popup selection bullets

--- js/script.js
+++ js/script.js
@@ -2,7 +2,15 @@ const body = document.querySelector('body')
 const popup = document.querySelector('.popup')
 const popupOpenBtn = document.querySelector('.result__btn--active')
 const popupCloseBtn = document.querySelector('.popup__close')
+const popupSelector = document.querySelector('.popup__selector')
+const popupSelectorBullets = document.querySelectorAll('.popup__circle')
 
+function deleteActiveBullet(){
+    let currentActiveBullet = popupSelector.querySelector('.popup__circle--active');
+    if (currentActiveBullet !== null){
+        currentActiveBullet.classList.remove('popup__circle--active')
+    }
+}
 
 popupOpenBtn.addEventListener('click', function(event){
     body.style.overflowY = 'hidden';
@@ -11,5 +19,14 @@ popupOpenBtn.addEventListener('click', function(event){
 
 popupCloseBtn.addEventListener('click', function(event){
     popup.classList.remove('popup--active');
+    deleteActiveBullet();
     body.removeAttribute('style');
+
+});
+
+popupSelectorBullets.forEach(function(bullet){
+    bullet.addEventListener('click', function(){
+        deleteActiveBullet();
+        bullet.classList.add('popup__circle--active');
+    });
 });
